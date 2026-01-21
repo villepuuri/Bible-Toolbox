4ec6fca83c0bec9d5605e61b7e275bd7f7307232
Ville Puuri <v.puuri@gmail.com>
2026-01-21 10:38:14 +0200

    Improved how the quotes are dispalayed

--- lib/data/services/api_text_cleaner.dart
+++ lib/data/services/api_text_cleaner.dart
@@ -16,10 +16,9 @@ class ApiTextCleaner {
     return result;
   }
 
-  /// Cleaner for pages
+  /// Cleaner function for pages
   static String cleanPage(String raw, {PageType? pageType}) {
     // String result = cleanPageLink(raw);
-    debugPrint("- Cleaning");
     pageType ??= PageType.other;
 
     // Fix the line breaks
@@ -113,7 +112,7 @@ class ApiTextCleaner {
     debugPrint(' - Cleaning Answer links');
 
     // Extract the question blocks from the data
-    RegExp blockRE = RegExp(r'(\[[\s\S]*?(\r\n){1,})', multiLine: true);
+    RegExp blockRE = RegExp(r'(\[[\s\S]*?(\r\n)+)', multiLine: true);
 
     // Get the start and end indexes
     Iterable<RegExpMatch> allMatches = blockRE.allMatches(raw);
@@ -130,29 +129,21 @@ class ApiTextCleaner {
 
   /// Change the possible HTML link to markdown syntax
   static String cleanLink(String raw) {
-    int htmlIndex = raw.indexOf("<hr>"); // todo: maybe change this
-    if (htmlIndex != -1) {
-      String htmlLink = raw.substring(0, htmlIndex);
+    // Regular Expression to detect the link items
+    final linkRegex = RegExp(
+      r'<a\s+href=\\*"([^"]+)\\*"[^>]*>(.*?)</a>',
+      caseSensitive: false,
+    );
 
-      // Regular Expression to detect the link items
-      final linkRegex = RegExp(
-        r'<a\s+href="([^"]+)"[^>]*>(.*?)</a>',
-        caseSensitive: false,
-      );
-
-      // Changing the html link to markdown link
-      String mdLink = htmlLink.replaceAllMapped(linkRegex, (match) {
-        final url = match.group(1)!;
-        final text = match.group(2)!;
-        return '[$text]($url)';
-      });
-
-      return "$mdLink\n---\n${raw.substring(htmlIndex + 4)}";
-    }
-    return raw;
+    // Changing the html link to markdown link
+    return raw.replaceAllMapped(linkRegex, (match) {
+      final url = match.group(1)!;
+      final text = match.group(2)!;
+      return '[$text]($url)';
+    });
   }
 
-  /// Remove opening <FONT ...> and closing </FONT>
+  /// Remove opening \<FONT ...> and closing \</FONT>
   static String cleanFont(String raw) {
     // Case insensitive, also handles attributes
     final fontRegex = RegExp(r'<\s*FONT[^>]*>', caseSensitive: false);
@@ -164,17 +155,22 @@ class ApiTextCleaner {
   /// Fix the line brakes in the quote blocks
   static String fixQuoteBlocks(String raw) {
     // Try to fix the line brakes in the quote block
-    final regex = RegExp(r'\r\n>([\s\S]*?)\r\n\r\n', multiLine: true);
+    final regex = RegExp(r'\r\n\s*>([\s\S]*?)((\r\n){2,}|$)', dotAll: true);
     return raw.replaceAllMapped(regex, (match) {
       // final full = match.group(0)!;       // whole matched quote block
       final inner = match.group(1)!; // text inside the block (after '>')
 
       // Removes unnecessary ">" marks
-      var convertedInner = inner.replaceAll('>', '');
-      // Replace CRLF inside the block
+      var convertedInner = inner.replaceAll('\r\n>', '');
+      // Replace line breaks to be fixed later with custom solution
       convertedInner = convertedInner.replaceAll('\r\n', '<br>');
 
       // Rebuild the block with replaced line breaks
+      debugPrint('*******');
+      debugPrint(inner);
+      debugPrint('*******');
+
+      // return '\r\n>$inner\r\n\r\n';
       return '\r\n>$convertedInner\r\n\r\n';
     });
   }
